4aa952d5a44d89774c657df0ab1af8793210c723
Shiyuan Zhang <695628449@qq.com>
2023-09-04 12:50:15 +0800

    Fixed bug

--- Greeter.java
+++ Greeter.java
@@ -2,24 +2,30 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class Greeter {
-
   private Locale locale;
   private String name;
 
   public Greeter(String language, String country, String name) {
-    locale = new Locale(language, country);
+    this.locale = new Locale(language, country);
     this.name = name;
   }
 
   public String sayHello() {
     ResourceBundle messages = ResourceBundle.getBundle("MessagesBundle", locale);
-    return messages.getString("greeting") + ", " + name;
+    String greeting = messages.getString("greeting");
+    return greeting + ", " + name;
   }
 
   public static void main(String[] args) {
+    if (args.length < 3) {
+      System.out.println("The number of command line arguments is invalid.");
+      return;
+    }
+
     String language = args[0];
     String country = args[1];
-    String name = args[2];
+    String name = String.join(" ", Arrays.copyOfRange(args, 2, args.length));
+
     Greeter greeter = new Greeter(language, country, name);
     System.out.println(greeter.sayHello());
   }
